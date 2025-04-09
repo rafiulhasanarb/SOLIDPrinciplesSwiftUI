41aaaecfe250673a4e01e35f7c9e2fad09b62ed0
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-04-09 23:17:21 +0600

    Initial Commit

---
+++ SOLIDPrinciplesSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOLIDPrinciplesSwiftUI
+//
+//  Created by Rafiul Hasan on 4/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOLIDPrinciplesSwiftUI/SOLIDPrinciplesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLIDPrinciplesSwiftUIApp.swift
+//  SOLIDPrinciplesSwiftUI
+//
+//  Created by Rafiul Hasan on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SOLIDPrinciplesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
